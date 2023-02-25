902b7a05137f3f0e51b53fef109ebec80959d258
Vladimir <fbvbnc@gmail.com>
2023-02-25 13:26:22 +0300

    EEE BOY

---
+++ src/main/java/com/example/try1231231/AppConfig.java
@@ -0,0 +1,19 @@
+package com.example.try1231231;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import weka.core.Attribute;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Attribute.class, new AttributeDeserializer());
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
+}
---
+++ src/main/java/com/example/try1231231/AttributeDeserializer.java
@@ -0,0 +1,37 @@
+package com.example.try1231231;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import weka.core.Attribute;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttributeDeserializer extends JsonDeserializer<Attribute> {
+    @Override
+    public Attribute deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        ObjectCodec oc = jp.getCodec();
+        JsonNode node = oc.readTree(jp);
+
+        String name = node.get("name").asText();
+        String type = node.get("type").asText();
+
+        if ("numeric".equals(type)) {
+            return new Attribute(name);
+        } else if ("nominal".equals(type)) {
+            List<String> values = new ArrayList<>();
+            JsonNode valuesNode = node.get("values");
+            for (JsonNode valueNode : valuesNode) {
+                values.add(valueNode.asText());
+            }
+            return new Attribute(name, values);
+        } else {
+            throw new RuntimeException("Unsupported attribute type: " + type);
+        }
+    }
+}
